17e0c073b8bbe3ac56a471be756cd9df5063edf7
Віктор Мінайленко <ipz224_mvyu@student.ztu.edu.ua>
2024-01-29 20:29:19 +0200

    Created .gitignore and console project

---
+++ ConsoleAppPrakt/ConsoleAppPrakt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace ConsoleAppPrakt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Console.Write("Введіть перше число: ");
+            if (double.TryParse(Console.ReadLine(), out double num1))
+            {
+                Console.Write("Введіть друге число:");
+                if (double.TryParse(Console.ReadLine(), out double num2))
+                {
+                    double sum = num1 + num2;
+                    Console.WriteLine($"{num1} + {num2} = {sum}");
+                }
+                else
+                {
+                    Console.WriteLine("Має бути правильний формат другого числа!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Має бути правильний формат першого числа!");
+            }
+
+        }
+    }
+}
